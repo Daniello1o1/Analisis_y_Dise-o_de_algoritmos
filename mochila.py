7b6e330048bea4e3543972abd9b65d8f34996125
Daniello1o1 <144377422+Daniello1o1@users.noreply.github.com>
2023-11-09 11:35:27 -0600

    Add files via upload

---
+++ mochila.py
@@ -0,0 +1,37 @@
+import time, random
+def mochila_fraccionaria(capacidad, objetos):
+    #Ordenar por valor/peso
+    objetos.sort(key = lambda x: x[0]/x[1], reverse = True)
+    total_objetos = 0.0
+    mochila = []
+    for valor, peso in objetos:
+        #Si el objeto se puede guardar completo
+        if capacidad >= peso:
+            mochila.append((valor,peso))
+            total_objetos += valor
+            capacidad -= peso
+        else:
+            fracc = capacidad / peso
+            mochila.append((valor * fracc, peso * fracc))
+            total_objetos += valor * fracc
+            break
+    return total_objetos, mochila
+def genlist():
+    objetos = []
+    capacidad = random.randint(1, 40)
+    for i in range (0,1000):
+        for j in range (0,2):
+            objetos.append((random.randint(1,20),random.randint(1,20)))
+    return capacidad, objetos
+def taim(): 
+    inicio = time.time()
+    capacidad, objetos = genlist()
+    mochila_fraccionaria(capacidad, objetos)
+    fin = time.time()
+    dif = fin - inicio
+    tiempos.append(dif)
+if __name__ == '__main__':
+    tiempos = []
+    for i in range(0,100):
+        taim()
+    print(tiempos)
